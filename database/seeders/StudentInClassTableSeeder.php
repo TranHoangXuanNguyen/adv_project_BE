b99abce1cbc9ae5bd78c6176a38a49d28d824e5d
Mikazuki <hoangnguyendepgiai@gmail.com>
2025-05-08 00:15:35 +0700

    update seeder

--- database/seeders/StudentInClassTableSeeder.php
+++ database/seeders/StudentInClassTableSeeder.php
@@ -10,17 +10,12 @@ class StudentInClassTableSeeder extends Seeder
     {
         DB::table('student_in_class')->insert([
             [
-                'user_id' => 5,
+                'user_id' => 1,
                 'class_id' => 1,
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
-            [
-                'user_id' => 1,
-                'class_id' => 2,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+
         ]);
     }
 }
