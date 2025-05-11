d700f62a0c5cff41415fe7702a9324b2f95b4366
Mikazuki <hoangnguyendepgiai@gmail.com>
2025-05-11 21:33:25 +0700

    done refactor

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Repositories\Eloquent\AuthRepository;
 use App\Repositories\Eloquent\UserRepository;
+use App\Repositories\Interfaces\IAuthRepository;
 use App\Repositories\Interfaces\IUserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -14,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function register(): void
     {
         //
+        $this->app->bind(IAuthRepository::class, AuthRepository::class);
         $this->app->bind(IUserRepository::class, UserRepository::class);
     }
 
