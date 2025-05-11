613d73951e6c0d573141767d0bf7f01c11e4f8c9
Mikazuki <hoangnguyendepgiai@gmail.com>
2025-05-11 21:33:25 +0700

    done refactor

--- app/Repositories/Eloquent/AuthRepository.php
+++ app/Repositories/Eloquent/AuthRepository.php
@@ -1,34 +1,41 @@
 <?php
+
 namespace App\Repositories\Eloquent;
 
-use App\Repositories\Interfaces\IUserRepository;
 use App\Models\User;
+use App\Repositories\Interfaces\IAuthRepository;
 
-class UserRepository implements IUserRepository
+class AuthRepository implements IAuthRepository
 {
     protected $model;
+
     public function __construct(User $model)
     {
         $this->model = $model;
     }
+
     public function getAll()
     {
         return $this->model->all();
     }
+
     public function getById($id)
     {
         return $this->model->findOrFail($id);
     }
+
     public function create(array $data)
     {
         return $this->model->create($data);
     }
+
     public function update($id, array $data)
     {
         $record = $this->model->findOrFail($id);
         $record->update($data);
         return $record;
     }
+
     public function delete($id)
     {
         $record = $this->model->findOrFail($id);
