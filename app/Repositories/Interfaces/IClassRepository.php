94d82812a705e5684e1ec3cb7adbaed96420f097
Mikazuki <hoangnguyendepgiai@gmail.com>
2025-05-16 23:49:57 +0700

    sai lam tuoi tre

--- app/Repositories/Interfaces/IClassRepository.php
+++ app/Repositories/Interfaces/IClassRepository.php
@@ -3,7 +3,6 @@ namespace App\Repositories\Interfaces;
 
 interface IClassRepository {
     public function getAll();
-    public function getById($id);
     public function create(array $data);
 
     public function update(int $id, array $data);
