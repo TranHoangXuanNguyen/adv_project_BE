683147ce213fcb4686ee8e3bcd76dcabb3e33572
Mikazuki <hoangnguyendepgiai@gmail.com>
2025-05-18 20:52:29 +0700

    done up progress

--- app/Repositories/Interfaces/IWeeklyRepository.php
+++ app/Repositories/Interfaces/IWeeklyRepository.php
@@ -6,8 +6,8 @@ interface IWeeklyRepository
 {
     public function getByid(int $id);
     public function createWeeklyTracking(array $data);
-
     public function getClassPlan(int $id,int $week_track_id);
     public function getSelfPlan(int $id,int $semesters_id,int $week_track_id);
+    public function updateWeeklyGoalStatus(int $id);
 
 }
